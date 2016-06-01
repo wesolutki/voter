dd7d25e8471522b18077e2a8fd6e0de594c21c28
Grzegorz Terlikowski <grzegorz.terlikowski@nokia.com>
2016-06-01 23:38:48 +0200

    prosty model

--- voter/models.py
+++ voter/models.py
@@ -1,5 +1,33 @@
+class Wspolnota(models.Model):
+    nazwa = models.CharField(max_length = 255, blank = True, null = True)
+    adres = models.TextField()
+
 
 class Lokal(models.Model):
+    wspolnota = models.ForeignKey(Wspolnota)
+    numer = models.CharField(max_length = 64, blank = True, null = True)
+
+
+class Udzial(models.Model):
     pass
 
-class 
+
+class Czlonek(models.Model):
+    pass
+
+
+class Uchwala(models.Model):
+    pass
+
+
+class Glos(models.Model):
+    pass
+
+
+class DraftUchwaly(models.Model):
+    pass
+
+
+class Komentarz(models.Model):
+    pass
+
